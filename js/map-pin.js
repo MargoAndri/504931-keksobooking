6e864279f28104e473527879eff4d768d92c18a7
MargoAndri <margoandriasyan@gmail.com>
2018-12-18 02:04:18 +0300

    Добавлены фильтры и debounce

--- js/map-pin.js
+++ js/map-pin.js
@@ -4,6 +4,13 @@
 (function () {
   var map = document.querySelector('.map');
   var pinButton = document.querySelector('.map__pin--main');
+  var filtersForm = document. querySelector('.map__filters');
+  var housingType = document.querySelector('#housing-type');
+  var housingPrice = document.querySelector('#housing-price');
+  var housingRoom = document.querySelector('#housing-rooms');
+  var housingGuestsNumber = document.querySelector('#housing-guests');
+  var housingFeatures = document.querySelectorAll('.map__checkbox');
+  var loadedAdvertisements = [];
   /**
    * @param {Object} advertisement
    * @param {Object} advertisement.author
@@ -23,28 +30,89 @@
     return pin;
   };
 
+  // Фильтр объявлений
+  /**
+   * @param {Array} advertisements
+   * @return {Array}
+   */
+  var filterAdvertisement = function (advertisements) {
+    var selectedHousingFeatures = [];
+    housingFeatures.forEach(function (item) {
+      if (item.checked) {
+        selectedHousingFeatures.push(item.value);
+      }
+    });
+    var advWithOffer = advertisements.filter(function (item) {
+      return 'offer' in item;
+    });
+    if (housingType.value !== 'any') {
+      advWithOffer = advWithOffer.filter(function (item) {
+        return item.offer.type === housingType.value;
+      });
+    }
+    if (housingPrice.value !== 'any') {
+      advWithOffer = advWithOffer.filter(function (item) {
+        switch (housingPrice.value) {
+          case 'low':
+            return item.offer.price < 10000;
+          case 'middle':
+            return item.offer.price >= 10000 && item.offer.price < 50000;
+          case 'high':
+            return item.offer.price >= 50000;
+        }
+        return false;
+      });
+
+    }
+    if (housingRoom.value !== 'any') {
+      advWithOffer = advWithOffer.filter(function (item) {
+        return item.offer.rooms === housingRoom.value;
+      });
+    }
+    if (housingGuestsNumber.value !== 'any') {
+      advWithOffer = advWithOffer.filter(function (item) {
+        return item.offer.guests === parseInt(housingGuestsNumber.value, 10);
+      });
+    }
+    advWithOffer = advWithOffer.filter(function (item) {
+      for (var i = 0; i < selectedHousingFeatures.length; i++) {
+        if (item.offer.features.indexOf(selectedHousingFeatures[i]) === -1) {
+          return false;
+        }
+      }
+      return true;
+    });
+    return advWithOffer.slice(0, window.data.ADV_QUANTITY);
+  };
+
+
   // Отрисовка меток в блоке карты
 
   /**
    * @param {Array} advertisements
    */
   var renderPins = function (advertisements) {
-    var advWithOffer = advertisements.filter(function (item) {
-      return 'offer' in item;
-    });
     var template = document.querySelector('#pin');
     var mapPins = document.querySelector('.map__pins');
     var allPins = document.querySelectorAll('.map__pin');
     if (allPins.length === 1) {
-      for (var i = 0; i < advWithOffer.length; i++) {
-        var pin = generatePin(advWithOffer[i], template);
+      for (var i = 0; i < advertisements.length; i++) {
+        var pin = generatePin(advertisements[i], template);
         mapPins.appendChild(pin);
       }
     }
     var mapPin = document.querySelectorAll('.map__pin');
     for (i = 1; i < mapPin.length; i++) {
-      window.page.addShowCardListener(mapPin[i], advWithOffer[i - 1]);
+      window.page.addShowCardListener(mapPin[i], advertisements[i - 1]);
     }
+  };
+
+  // Активация формы фильтров
+  var activateFilterForm = function () {
+    var filterSelector = filtersForm.querySelectorAll('.map__filter');
+    var filterFeatures = filtersForm.querySelector('fieldset');
+    window.form.enableElements(filterSelector);
+    filterFeatures.removeAttribute('disabled');
   };
 
   // drag-and-drop
@@ -59,14 +127,9 @@
     var activatePage = function () {
       var form = document.querySelector('.ad-form');
       var formSpace = form.querySelectorAll('fieldset');
-      var filtersForm = document.querySelector('.map__filters');
-      var filterSelector = filtersForm.querySelectorAll('.map__filter');
-      var filterFeatures = filtersForm.querySelector('fieldset');
       map.classList.remove('map--faded');
       form.classList.remove('ad-form--disabled');
       window.form.enableElements(formSpace);
-      window.form.enableElements(filterSelector);
-      filterFeatures.removeAttribute('disabled');
     };
     /**
      * @param {Object} moveEvt
@@ -100,8 +163,7 @@
      */
     var onMouseUp = function (upEvt) {
       upEvt.preventDefault();
-      window.backend.load(renderPins, alert);
-
+      window.backend.load(successHandler, alert);
       document.removeEventListener('mousemove', onMouseMove);
       document.removeEventListener('mouseup', onMouseUp);
       window.form.updateAddressField();
@@ -112,4 +174,22 @@
     document.addEventListener('mouseup', onMouseUp);
 
   });
+  /**
+   * @param {Array} adv
+   */
+  var successHandler = function (adv) {
+    loadedAdvertisements = adv;
+    var filtered = filterAdvertisement(loadedAdvertisements);
+    renderPins(filtered);
+    activateFilterForm();
+  };
+
+  filtersForm.addEventListener('change', function () {
+    window.debounce(function () {
+      var filtered = filterAdvertisement(loadedAdvertisements);
+      window.page.clearMap();
+      renderPins(filtered);
+    });
+  });
+
 })();
